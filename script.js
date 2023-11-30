2086d4a134f486f7112a587ba036d2f71c65c592
Ben Procknow <benp8282@gmail.com>
2023-11-29 20:20:29 -0600

    Update js script to use json format per backend

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 document.getElementById('startTimer').addEventListener('click', function() {
     fetch('http://127.0.0.1:5000/requestData', { method: 'GET' })
-    .then(response => response.text())
+    .then(response => response.json())  // Make sure to use .json() if the response is JSON
     .then(data => {
-        document.getElementById('timerDisplay').innerText = data;
+        // Access the 'message' field from the data
+        document.getElementById('timerDisplay').innerText = data.message;
+    })
+    .catch(error => {
+        console.error('Error:', error);
     });
 });
 
 document.getElementById('stopTimer').addEventListener('click', function() {
-    fetch('http://127.0.0.1:5000/submitResponse', { method: 'POST' })
-    .then(response => response.text())
+    fetch('http://127.0.0.1:5000/submitResponse', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ someData: 'someDataHere' })  // Send data as JSON
+    })
+    .then(response => response.json())
     .then(data => {
-        document.getElementById('timerDisplay').innerText = data;
+        document.getElementById('timerDisplay').innerText = data.status;
+    })
+    .catch(error => {
+        console.error('Error:', error);
     });
 });
+
